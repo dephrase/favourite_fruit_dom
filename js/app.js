40014e40c137225ea473967c8ddeb4d956740009
Dephrase <davidjf123@gmail.com>
2021-06-11 14:49:09 +0100

    Adding basic styling

--- js/app.js
+++ js/app.js
@@ -7,25 +7,24 @@ document.addEventListener('DOMContentLoaded', ()=> {
 function addNewFruit(event){
     event.preventDefault();
 
-    const fruitName = event.target.name.value;
-    const fruitColour = event.target.colour.value;
-    const favouriteFruit = event.target.favourite.value;
-
     const newListItem = document.createElement('ul')
     const newListName = document.createElement('li')
-    newListName.textContent = fruitName
+    newListName.textContent = event.target.name.value;
+    newListName.className = "subListItem"
     const newListColour = document.createElement('li')
-    newListColour.textContent = fruitColour
+    newListColour.textContent = event.target.colour.value;
+    newListColour.className = "subListItem"
     const newListFavourite = document.createElement('li')
-    newListFavourite.textContent = favouriteFruit
+    newListFavourite.textContent = event.target.favourite.value;
+    newListFavourite.className = "subListItem"
 
     newListItem.append(newListName)
     newListItem.append(newListColour)
     newListItem.append(newListFavourite)
 
-    document.querySelector('#new-fruit-form').append(newListItem)
+    document.querySelector('#fruit-list').append(newListItem)
 
-
+    event.target.reset();
 }
 
 
