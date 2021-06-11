2ff885c51032eb24b26c25b1020db0b1de500061
Dephrase <davidjf123@gmail.com>
2021-06-11 13:16:05 +0100

    Able to add new fruit with name, colour and favourite from form to list of fruits

--- js/app.js
+++ js/app.js
@@ -1,10 +1,31 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     const fruitList = document.querySelector('#fruit-list')
-    const submitButton = document.querySelector('#save')
-    submitButton.addEventListener('click', addNewFruit(event))
+    document.querySelector('#new-fruit-form').addEventListener('submit', addNewFruit)
 
 })
 
 function addNewFruit(event){
-    console.log(event)
+    event.preventDefault();
+
+    const fruitName = event.target.name.value;
+    const fruitColour = event.target.colour.value;
+    const favouriteFruit = event.target.favourite.value;
+
+    const newListItem = document.createElement('ul')
+    const newListName = document.createElement('li')
+    newListName.textContent = fruitName
+    const newListColour = document.createElement('li')
+    newListColour.textContent = fruitColour
+    const newListFavourite = document.createElement('li')
+    newListFavourite.textContent = favouriteFruit
+
+    newListItem.append(newListName)
+    newListItem.append(newListColour)
+    newListItem.append(newListFavourite)
+
+    document.querySelector('#new-fruit-form').append(newListItem)
+
+
 }
+
+
